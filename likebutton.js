3b42a902877b15675cde7b8be4cbae366c765bfc
Unknown <mj0857363@otc.edu>
2017-09-27 13:18:53 -0500

    Things?

--- likebutton.js
+++ likebutton.js
@@ -3,8 +3,8 @@ let incrementbutton = Array.from(document.getElementsByClassName("button-increme
 incrementbutton.forEach(btn => {
     btn.addEventListener("click", e => {
         let count = btn.nextSibling.textContent.trim()
-        newCount = parceInt(count) + 1
-        btn.nextSibling.textContent = newCount
+        let newCount = parceInt(count) + 1
+        count = newCount
     });
 });
 
