f935232760655b8eb48ddca9002f2abe6d0521d2
Unknown <mj0857363@otc.edu>
2017-10-02 12:00:56 -0500

    It works!

--- likebutton.js
+++ likebutton.js
@@ -1,10 +1,11 @@
-let incrementbutton = Array.from(document.getElementsByClassName("button-increment"))
+let incrementbutton = Array.from(document.getElementsByClassName("button-inc"))
 
 incrementbutton.forEach(btn => {
     btn.addEventListener("click", e => {
         let count = btn.nextSibling.textContent.trim()
-        let newCount = parceInt(count) + 1
-        count = newCount
+        let newCount = parseInt(count) + 1
+        
+        btn.nextSibling.textContent = newCount;
     });
 });
 
